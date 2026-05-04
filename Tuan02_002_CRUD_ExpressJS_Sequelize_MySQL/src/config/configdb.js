9d9020d11d2dba806f49c9f7c8880271080e0d5e
KickHuynh <huynhngoctai205@gmail.com>
2026-05-04 13:53:19 +0700

    Add CRUD Express + Sequelize MySQL app
    Add a Week 02 CRUD application scaffold implementing Student CRUD with Express, Sequelize and MySQL. Includes MVC components (models, migration, controller, service, routes, EJS views), server and view engine setup, config files (.sequelizerc, config, .env.example), Babel config, .gitignore and README documenting the project. package.json added with runtime and dev dependencies to run and develop the app.

---
+++ Tuan02_002_CRUD_ExpressJS_Sequelize_MySQL/src/config/configdb.js
@@ -0,0 +1,23 @@
+import { Sequelize } from 'sequelize';
+
+const sequelize = new Sequelize(
+  'student_crud_db',
+  'root',
+  'Ngoctai2005@@',
+  {
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    logging: false
+  }
+);
+
+const connectDB = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+};
+
+export default connectDB;
